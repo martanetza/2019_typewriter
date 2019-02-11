ee2f371d7cdc21d628c59b26ccdf360fee77f629
martanetza <39929716+martanetza@users.noreply.github.com>
2019-02-11 17:56:32 +0100

    my solution

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,31 @@
 "use strict";
+const text = document.querySelector("#typewriter").textContent;
+console.log(text);
+function init() {
+  removeText();
+}
+
+function removeText() {
+  document.querySelector("#typewriter").textContent = " ";
+
+  writeText();
+}
+function writeText() {
+  let i;
+  for (i = 0; i <= text.length; i++) {
+    (function(i) {
+      setTimeout(function() {
+        document.querySelector("#typewriter").textContent = text.slice(0, i);
+        let snd = document.getElementById("typekey2");
+        snd.play();
+        if (text[i] === " ") {
+          let sndSpace = document.getElementById("typespace");
+          sndSpace.play();
+          snd.stop();
+        }
+      }, 400 * i);
+    })(i);
+  }
+}
+
+init();
