743c9f597c6f0427f1d770f833177b1320a096b7
martanetza <39929716+martanetza@users.noreply.github.com>
2019-02-12 09:26:07 +0100

    no loop just if

--- typewriter.js
+++ typewriter.js
@@ -10,24 +10,46 @@ function removeText() {
 
   writeText();
 }
+// function writeText() {
+//   for (let i = 0; i <= text.length; i++) {
+//     (function(i) {
+//       setTimeout(function() {
+//         document.querySelector("#typewriter").textContent = text.slice(0, i);
+//         let snd = document.getElementById("typekey1");
+//         snd.play();
+//         if (text[i] === " ") {
+//           let snd = document.getElementById("typespace");
+//           snd.play();
+//         }
+//         console.log(i);
+//         if (i == text.length) {
+//           writeText();
+//         }
+//       }, 400 * i);
+//     })(i);
+//   }
+// }
+
+let i = 0;
+console.log(i);
+
 function writeText() {
-  for (let i = 0; i <= text.length; i++) {
-    (function(i) {
-      setTimeout(function() {
-        document.querySelector("#typewriter").textContent = text.slice(0, i);
-        let snd = document.getElementById("typekey1");
+  setTimeout(function() {
+    if (i <= text.length) {
+      document.querySelector("#typewriter").textContent = text.slice(0, i);
+      i++;
+      let snd = document.getElementById("typekey1");
+      if (text[i] === " ") {
+        let snd = document.getElementById("typespace");
         snd.play();
-        if (text[i] === " ") {
-          let snd = document.getElementById("typespace");
-          snd.play();
-        }
-        console.log(i);
-        if (i == text.length) {
-          writeText();
-        }
-      }, 400 * i);
-    })(i);
-  }
+      }
+      console.log(i);
+      if (i == text.length) {
+        i = 0;
+      }
+      writeText();
+    }
+  }, 400);
 }
 
 init();
